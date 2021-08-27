10f796e2fc51081903ac3b3ae4f34120d403fd31
cds <atguigu@qq.com>
2021-08-28 02:03:13 +0800

    hotfix commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello atguigu");
         System.out.println("sh0426");
+        System.out.println("hotfix");
     }
 }
